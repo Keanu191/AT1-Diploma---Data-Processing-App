f184df12ce7341e51944b70dd9d1a63b47bbb19a
KEANU FARRO <30074191@tafe.wa.edu.au>
2024-07-23 14:51:03 +0800

    confused on what to do for the loop

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,6 +8,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Collections.Generic; // import (4.1)
+using System.IO;
 
 namespace AT1Dip
 {
@@ -19,6 +21,49 @@ namespace AT1Dip
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        /* 4.1 Create two data structures using the LinkedList<T> class from the C# Systems.Collections.Generic
+         * namespace. The data must be of type "double". The two LinkedLists of type double are to be declared as global
+         * within the "public partial class".
+         */
+        LinkedList<double> sensorA = new LinkedList<double>();
+        LinkedList<double> sensorB = new LinkedList<double>();
+
+        /*
+         * 4.2 Copy the Galileo.DLL file into the root directory of your solution folder and add the appropiate
+         * reference in the solution explorer. Create a method called "LoadData" which will populate both 
+         * LinkedLists. Declare an instance of the Galileo library in the method and create the appropirate
+         * loop construct to populate the two LinkedList; the data from Sensor A will populate the first LinkedList,
+         * while the data from Sensor B will populate the second LinkedList. The LinkedList size will be hardcoded
+         * inside the method and must be equal to 400. The input parameters are empty, and the return type is void.
+         */
+
+        public void LoadData()
+        {
+            string galileoPath = "D:\\TAFE DIPLOMA\\Monday C# - Milan\\Projects\\AT1Dip\\Galileo6.dll";
+
+            if (File.Exists(galileoPath))
+            {
+                using (var stream = File.Open(galileoPath, FileMode.Open))
+                {
+                    foreach (double data in sensorA)
+                    {
+                        
+                        
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("ERROR: Cannot find the DLL file!");
+            }
+            
+        }
+
+        private void btnLoadAB_Click(object sender, RoutedEventArgs e)
+        {
+            LoadData(); // call load data method
         }
     }
 }
